7356440ebdf3d2ffa92918e9796c976ff5b86c61
Rpt87gtm <rpt87gtm@bk.ru>
2024-11-24 19:16:06 +0800

    back hello world

---
+++ backend/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace llmChat.Controllers
+{
+    [ApiController]
+    [Route("/helloWorld")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Hello, World!";
+        }
+    }
+}
