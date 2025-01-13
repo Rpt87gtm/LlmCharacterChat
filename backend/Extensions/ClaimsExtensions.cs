85de3eb07e00cf3b13457839adccf7f8184f2184
Rpt87gtm <rpt87gtm@bk.ru>
2025-01-13 15:29:42 +0800

    some tests

--- backend/Extensions/ClaimsExtensions.cs
+++ backend/Extensions/ClaimsExtensions.cs
@@ -6,7 +6,18 @@ namespace api.Extensions
     {
         public static string GetUsername(this ClaimsPrincipal user)
         {
-            return user.Claims.SingleOrDefault(x => x.Type.Equals("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname")).Value;
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            var claim = user.Claims.SingleOrDefault(x => x.Type.Equals("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname"));
+            if (claim == null)
+            {
+                throw new InvalidOperationException("Claim not found");
+            }
+
+            return claim.Value;
         }
     }
 }
